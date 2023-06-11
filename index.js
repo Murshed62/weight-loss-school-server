db7aa5c3e002c97ade383eaf46f09d867352b0c2
Murshed62 <shesir.murshed@gmail.com>
2023-06-11 12:29:05 +0600

    mongoDB conected and user added in database

--- index.js
+++ index.js
@@ -3,16 +3,64 @@ const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const port = process.env.PORT ||5000;
+const port = process.env.PORT || 5000;
 
 // middleware
+
+console.log(process.env.DB_USER, process.env.DB_PASS);
+
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) =>{
-    res.send('weight lose progressing')
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cpvz2.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    const userCollection = client.db('slimFitAcademy').collection('users');
+
+    app.get('/users', async(req, res)=>{
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
+    app.post('/users', async(req,res)=>{
+      const user = req.body;
+      const query = {email: user.email}
+      const existingUser = await userCollection.findOne(query);
+      if(existingUser){
+        return res.send({message: 'user already exists'})
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req,res)=>{
+  res.send('slimfit academy running');
 })
 
-app.listen(port, () =>{
-    console.log(`weight loss is progressing on port: ${port}`);
+app.listen(port, ()=>{
+  console.log(`slimfit is running on port: ${port}`);
 })
